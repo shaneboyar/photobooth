0ca17a27272a1a25738fc086f871820728125fe2
Shane Boyar <shane.boyar@gmail.com>
2016-09-17 21:11:19 -0400

    Start figuring out fullscreen instructions

--- camera.py
+++ camera.py
@@ -3,6 +3,11 @@ from time import sleep
 import os
 import datetime
 from gpiozero import LED
+import pygame
+from pygame.locals import *
+
+#Miniscreen Resolution is 800x480
+
 
 c = PiCamera()
 led = LED(2)
@@ -14,6 +19,25 @@ if not os.path.exists(path):
     os.makedirs(path)
 
 c.rotation = 180
+pygame.init()
+modes = pygame.display.list_modes()
+pygame.display.set_mode(max(modes))
+screen = pygame.display.get_surface()
+pygame.display.set_caption('Photo Booth')
+pygame.mouse.set_visible(False)
+pygame.display.toggle_fullscreen()
+
+
+#img = pygame.image.load("intro.png")
+#img = img.convert
+#set_dimensions(img.get_width(), img.get_height())
+
+#img = pygame.transform.scale(img, (800, 480))
+#screen.blit(img,(0,0))
+#pygame.display.flip()
+
+sleep(5)
+pygame.quit()
 c.start_preview()
 sleep(2)
 for i in range(5):
